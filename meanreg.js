de85958306e56914025eb3a923b351858bcfaf1f
Jyotirmaya jena <102820151+jyotirmaya907@users.noreply.github.com>
2022-09-28 20:44:07 +0530

    Add files via upload

---
+++ meanreg.js
@@ -0,0 +1,19 @@
+function add(){
+    document.getElementById("f1").value;
+    document.getElementById("f1").innerHTML;
+    document.write()
+}
+function clr(){
+    document.getElementById("f1").value="";  
+    document.getElementById("f2").value=""; 
+    document.getElementById("f3").value=""; 
+    document.getElementById("f4").value=""; 
+    document.getElementById("f5").value=""; 
+    document.getElementById("f6").value=""; 
+    document.getElementById("f7").value=""; 
+    document.getElementById("f8").value=""; 
+    document.getElementById("f1").focus();
+} 
+function Ald(){
+    alert("server busy")
+}
